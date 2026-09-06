d198946b93d935ee0d7fab07e244949ffd767e3e
samanandpure <139612681+samanandpure@users.noreply.github.com>
2026-09-06 21:32:47 +0530

    Create ddl.sql

---
+++ scripts/ddl.sql
@@ -0,0 +1,74 @@
+/*
+it create the tables in the bronze layer for crm & erp, 3 tables each
+
+1) The code will check the whether table exists or not, if it exists then it will drop it
+2) it will create the table
+3) if object_id is present in user defined 
+*/
+IF	OBJECT_ID ('bronze.crm_cust_info','U') IS NOT NULL
+	DROP TABLE bronze.crm_cust_info;
+
+CREATE TABLE bronze.crm_cust_info (
+	cst_id INT,
+	cst_key VARCHAR(50),
+	cst_firstname VARCHAR(50),
+	cst_lastname VARCHAR(50),
+	cst_marital_status VARCHAR(50),
+	cst_gndr VARCHAR(50),
+	cst_create_date DATE
+);
+
+IF OBJECT_ID('bronze.crm_prd_info','U') is not null
+	DROP TABLE bronze.crm_prd_info
+CREATE TABLE bronze.crm_prd_info (
+	prd_id INT,
+	prd_key VARCHAR(50),
+	prd_nm VARCHAR(50),
+	prd_cost INT,
+	prd_line VARCHAR(50),
+	prd_start_dt DATETIME,
+	prd_end_dt DATETIME
+);
+
+IF OBJECT_ID('bronze.crm_sales_details','U') IS NOT NULL
+	DROP TABLE bronze.crm_sales_details;
+
+CREATE TABLE bronze.crm_sales_details (
+	sls_ord_num VARCHAR(50),
+	sls_prd_key VARCHAR(50),
+	sls_cust_id INT,
+	sls_order_dt int,
+	sls_ship_dt int,
+	sls_due_dt int,
+	sls_sales FLOAT,
+	sls_quantity INT,
+	sls_price INT
+);
+
+
+IF OBJECT_ID('bronze.erp_cust_az12','U') IS NOT NULL
+	DROP TABLE bronze.erp_cust_az12;
+
+CREATE TABLE bronze.erp_cust_az12 (
+	CID VARCHAR(50),
+	BDATE DATE,
+	GEN VARCHAR(50)
+);
+
+IF OBJECT_ID('bronze.erp_loc_a101','U') IS NOT NULL
+	DROP TABLE bronze.erp_loc_a101;
+
+CREATE TABLE bronze.erp_loc_a101 (
+	CID varchar(50),
+	CNTRY varchar(50)
+);
+
+IF OBJECT_ID('bronze.erp_px_cat_g1v2','U') IS NOT NULL
+	DROP TABLE bronze.erp_px_cat_g1v2;
+
+CREATE TABLE bronze.erp_px_cat_g1v2 (
+	ID VARCHAR(50),
+	CAT VARCHAR(50),
+	SUBCAT VARCHAR(50),
+	MAINTENANCE VARCHAR(50)
+);
