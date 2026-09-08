50ed12cb6ad78bb6a30153c8c0e3b083f7f2bbd0
samanandpure <139612681+samanandpure@users.noreply.github.com>
2026-09-08 11:33:17 +0530

    Update silver_ddl.sql

--- scripts/silver/silver_ddl.sql
+++ scripts/silver/silver_ddl.sql
@@ -1,1 +1,81 @@
+/*
 
+
+1) created the same table structure as it is the bronze but added one column in each table of dwh_create_date
+and by default kept the default value of current by getdate()
+
+
+*/
+IF	OBJECT_ID ('silver.crm_cust_info','U') IS NOT NULL
+	DROP TABLE silver.crm_cust_info;
+
+CREATE TABLE silver.crm_cust_info (
+	cst_id INT,
+	cst_key VARCHAR(50),
+	cst_firstname VARCHAR(50),
+	cst_lastname VARCHAR(50),
+	cst_marital_status VARCHAR(50),
+	cst_gndr VARCHAR(50),
+	cst_create_date DATE,
+	dwh_create_date datetime default getdate()
+);
+
+IF OBJECT_ID('silver.crm_prd_info','U') is not null
+	DROP TABLE silver.crm_prd_info
+CREATE TABLE silver.crm_prd_info (
+	prd_id INT,
+	prd_key VARCHAR(50),
+	prd_nm VARCHAR(50),
+	prd_cost INT,
+	prd_line VARCHAR(50),
+	prd_start_dt DATETIME,
+	prd_end_dt DATETIME,
+	dwh_create_date datetime default getdate()
+);
+
+IF OBJECT_ID('silver.crm_sales_details','U') IS NOT NULL
+	DROP TABLE silver.crm_sales_details;
+
+CREATE TABLE silver.crm_sales_details (
+	sls_ord_num VARCHAR(50),
+	sls_prd_key VARCHAR(50),
+	sls_cust_id INT,
+	sls_order_dt int,
+	sls_ship_dt int,
+	sls_due_dt int,
+	sls_sales FLOAT,
+	sls_quantity INT,
+	sls_price INT,
+	dwh_create_date datetime default getdate()
+);
+
+
+IF OBJECT_ID('silver.erp_cust_az12','U') IS NOT NULL
+	DROP TABLE silver.erp_cust_az12;
+
+CREATE TABLE silver.erp_cust_az12 (
+	CID VARCHAR(50),
+	BDATE DATE,
+	GEN VARCHAR(50),
+	dwh_create_date datetime default getdate()
+);
+
+IF OBJECT_ID('silver.erp_loc_a101','U') IS NOT NULL
+	DROP TABLE silver.erp_loc_a101;
+
+CREATE TABLE silver.erp_loc_a101 (
+	CID varchar(50),
+	CNTRY varchar(50),
+	dwh_create_date datetime default getdate()
+);
+
+IF OBJECT_ID('silver.erp_px_cat_g1v2','U') IS NOT NULL
+	DROP TABLE silver.erp_px_cat_g1v2;
+
+CREATE TABLE silver.erp_px_cat_g1v2 (
+	ID VARCHAR(50),
+	CAT VARCHAR(50),
+	SUBCAT VARCHAR(50),
+	MAINTENANCE VARCHAR(50),
+	dwh_create_date datetime default getdate()
+);
