8cf0886710ed620f1411fb25a5622c27fdff00e7
samanandpure <139612681+samanandpure@users.noreply.github.com>
2026-09-13 12:56:28 +0530

    Update erp_tables.sql

--- scripts/silver/erp_tables.sql
+++ scripts/silver/erp_tables.sql
@@ -23,3 +23,21 @@ select
     END AS gen
 from 
     bronze.erp_cust_az12;
+
+
+/*
+
+*/
+
+insert into silver.erp_loc_a101 (
+	CID,
+	CNTRY)
+select
+	replace(CID,'-','') as CID,
+	CASE
+		WHEN TRIM(CNTRY) in ('US','USA') THEN 'United States'
+		WHEN TRIM(CNTRY) = 'DE' THEN 'Germany'
+		WHEN TRIM(CNTRY) is Null or TRIM(CNTRY) = '' THEN 'n/a'
+		ELSE TRIM(CNTRY)
+	END AS CNTRY
+from bronze.erp_loc_a101;
