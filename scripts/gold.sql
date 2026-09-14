ee408a6cd504b8861e1b83d35b4f234fa326a903
samanandpure <139612681+samanandpure@users.noreply.github.com>
2026-09-14 19:31:41 +0530

    Create gold.sql

---
+++ scripts/gold.sql
@@ -0,0 +1,60 @@
+create view gold.dim_customers as
+select
+	ROW_NUMBER() over (order by cst_id) as customer_key,
+	ci.cst_id as customer_id,
+	ci.cst_key as customer_number,
+	ci.cst_firstname as first_name,
+	ci.cst_lastname as last_name,
+	la.cntry as country,
+	ci.cst_marital_status as marital_status,
+	ci.cst_create_date as create_date,
+	case
+		when ci.cst_gndr != 'n/a' then  ci.cst_gndr
+		else coalesce(ca.GEN, 'n/a')
+	end as gender,
+	ca.BDATE as birthdate
+
+from
+	silver.crm_cust_info ci
+left join silver.erp_cust_az12 ca
+	on ci.cst_key = ca.CID
+left join silver.erp_loc_a101 la
+	on ci.cst_key = la.CID;
+
+
+
+CREATE VIEW gold.dim_products AS
+SELECT
+    ROW_NUMBER() OVER (ORDER BY pn.prd_start_dt, pn.prd_key) AS product_key,
+    pn.prd_id       AS product_id,
+    pn.prd_key      AS product_number,
+    pn.prd_nm       AS product_name,
+    pn.cat_id       AS category_id,
+    pc.cat          AS category,
+    pc.subcat       AS subcategory,
+    pc.maintenance  AS maintenance,
+    pn.prd_cost     AS cost,
+    pn.prd_line     AS product_line,
+    pn.prd_start_dt AS start_date
+FROM silver.crm_prd_info pn
+LEFT JOIN silver.erp_px_cat_g1v2 pc
+    ON pn.cat_id = pc.id
+WHERE pn.prd_end_dt IS NULL;
+
+
+CREATE VIEW gold.fact_sales AS
+SELECT
+    sd.sls_ord_num  AS order_number,
+    pr.product_key  AS product_key,
+    cu.customer_key AS customer_key,
+    sd.sls_order_dt AS order_date,
+    sd.sls_ship_dt  AS shipping_date,
+    sd.sls_due_dt   AS due_date,
+    sd.sls_sales    AS sales_amount,
+    sd.sls_quantity AS quantity,
+    sd.sls_price    AS price
+FROM silver.crm_sales_details sd
+LEFT JOIN gold.dim_products pr
+    ON sd.sls_prd_key = pr.product_number
+LEFT JOIN gold.dim_customers cu
+    ON sd.sls_cust_id = cu.customer_id;
