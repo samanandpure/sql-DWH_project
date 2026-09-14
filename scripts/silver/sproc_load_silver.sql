1bf05d2752e8a7df80296ae9503c95d52bd4fdc8
samanandpure <139612681+samanandpure@users.noreply.github.com>
2026-09-14 12:06:42 +0530

    Create sproc_load_silver.sql

---
+++ scripts/silver/sproc_load_silver.sql
@@ -0,0 +1,217 @@
+CREATE OR ALTER procedure silver.LOAD_SILVER AS
+BEGIN
+	BEGIN TRY
+		DECLARE @start_time DATETIME, @end_time DATETIME, @silver_load_start DATETIME, @silver_load_end DATETIME
+		print 'Truncating table crm_cust_info';
+
+		TRUNCATE TABLE silver.crm_cust_info;
+
+		set @silver_load_start = GETDATE();
+		SET  @start_time  = GETDATE();
+
+		PRINT 'INSERTING RECORDS INTO silver.crm_cuts_info';
+
+		insert into silver.crm_cust_info (
+			cst_id,
+			cst_key,
+			cst_firstname,
+			cst_lastname,
+			cst_marital_status,
+			cst_gndr,
+			cst_create_date)
+		select 
+			cst_id,
+			cst_key,
+			TRIM (cst_firstname), 
+			TRIM (cst_lastname) ,
+			case 
+				when TRIM(upper(cst_marital_status)) = 'M' then 'married'
+				when TRIM(upper(cst_marital_status)) = 'S' then 'single'
+				else 'n/a'
+			end as cst_marr_cnst,
+			case 
+				when TRIM(upper(cst_gndr)) = 'F' then 'female'
+				when TRIM(upper(cst_gndr)) = 'M' then 'male'
+				else 'n/a'
+			end as cst_gndr_cn,
+			cst_create_date
+		from (
+		select
+			 *,
+			 ROW_NUMBER() over (partition by cst_id order by cst_create_date) as rn
+		from 
+			 bronze.crm_cust_info) t
+		where rn = 1;
+
+		PRINT 'INSERTED RECORDS INTO silver.crm_cust_info';
+		SET  @end_time  = GETDATE();
+
+		print 'load duration: ' + cast(datediff(second, @start_time,@end_time) AS VARCHAR) + '   seconds';
+
+		PRINT 'TRUNCATING TABLE SILVER.crm_prd_info';
+
+		TRUNCATE TABLE silver.crm_prd_info;
+		PRINT 'inserting records in table silver.crm_prd_info';
+
+		SET @start_time = GETDATE()
+
+		insert into silver.crm_prd_info (
+			prd_id ,
+			cat_id ,
+			prd_key ,
+			prd_nm ,
+			prd_cost ,
+			prd_line ,
+			prd_start_dt,
+			prd_end_dt
+		)
+		select 
+			prd_id,
+			SUBSTRING (prd_key,1,5) as cat_id,
+			REPLACE (SUBSTRING(prd_key,7,len(prd_key)),'-','_'),
+			TRIM(prd_nm),
+			ISNULL(prd_cost,0) as prd_cost,
+			CASE TRIM(UPPER(prd_line))
+		WHEN 'M' THEN 'Mountain'
+		WHEN 'R' THEN 'Road'
+		WHEN 'S' THEN 'Other Sales'
+		WHEN 'T' THEN 'Touring'
+		ELSE 'N/A'
+			END AS prd_line,
+		CAST(prd_start_dt as DATE) as prd_start_dt,
+		CAST(((lead(prd_start_dt) over(partition by prd_key order by prd_start_dt)) - 1) as date) as prd_end_dt
+		from bronze.crm_prd_info;
+
+		SET @end_time = GETDATE();
+		print 'load duration: ' + cast(datediff(second, @start_time,@end_time) AS VARCHAR) + '   seconds';
+		
+
+		print 'truncating table silevr.crm_sales_details';
+		TRUNCATE TABLE silver.crm_sales_details;
+
+		print 'inserting the records into silver.crm_sales_details';
+
+		SET @start_time = GETDATE();
+
+		insert into silver.crm_sales_details (
+			sls_ord_num ,
+			sls_prd_key ,
+			sls_cust_id ,
+			sls_order_dt ,
+			sls_ship_dt ,
+			sls_due_dt ,
+			sls_sales ,
+			sls_quantity ,
+			sls_price 
+
+		)
+		select
+			sls_ord_num,
+			sls_prd_key,
+			sls_cust_id,
+			case 
+				when sls_order_dt < 0 or len(sls_order_dt) != 8 then null
+				else CAST(CAST(sls_order_dt AS VARCHAR ) AS DATE)
+			end as sls_order_dt,
+			case 
+				when sls_ship_dt < 0 or len(sls_ship_dt) != 8 then null
+				else CAST(CAST(sls_ship_dt AS VARCHAR ) AS DATE)
+			end as sls_ship_dt,
+			case 
+				when sls_due_dt < 0 or len(sls_due_dt) != 8 then null
+				else CAST(CAST(sls_due_dt AS VARCHAR ) AS DATE)
+			end as sls_due_dt,
+			case 
+				when sls_sales is null or sls_sales <= 0 or sls_sales != sls_quantity * ABS(sls_price) then sls_quantity * ABS(sls_price)
+				else sls_sales
+			end as sls_sales,
+			sls_quantity,
+			case 
+				when sls_price is null or sls_price <= 0 then sls_sales/ nullif (sls_quantity,0)
+				else sls_price
+			end as sls_price
+		from
+			bronze.crm_sales_details;
+
+		set @end_time = GETDATE();
+		print 'load duration: ' + cast(datediff(second, @start_time,@end_time) AS VARCHAR) + '   seconds';
+
+
+		print 'truncating table silver.erp_cust_az12';
+
+		TRUNCATE TABLE silver.erp_cust_az12;
+
+		SET @start_time = GETDATE();
+
+		insert into silver.erp_cust_az12 (
+			CID,
+			BDATE,
+			GEN
+		)
+		select
+			CASE 
+				WHEN CID LIKE 'NAS%' THEN SUBSTRING(CID,4,len(CID))
+				else CID
+			END AS CID,
+			CASE 
+				WHEN BDATE > GETDATE() THEN NULL
+				ELSE BDATE
+			END AS BDATE,
+			CASE
+				WHEN UPPER(TRIM(gen)) in ('F','FEMALE') THEN 'Female'
+				WHEN UPPER(TRIM(gen)) in ('M','MALE') THEN 'Male'
+				ELSE 'n/a'
+			END AS gen
+		from 
+			bronze.erp_cust_az12;
+
+		SET @end_time =GETDATE();
+		print 'load duration: ' + cast(datediff(second, @start_time,@end_time) AS VARCHAR) + '   seconds';
+
+		print 'truncating table silver.erp_loc_a101';
+
+		TRUNCATE TABLE silver.erp_loc_a101;
+
+		print 'inserting records into silver.erp_loc_a101';
+		set @start_time = GETDATE();
+
+		insert into silver.erp_loc_a101 (
+			CID,
+			CNTRY)
+		select
+			replace(CID,'-','') as CID,
+			CASE
+				WHEN TRIM(CNTRY) in ('US','USA') THEN 'United States'
+				WHEN TRIM(CNTRY) = 'DE' THEN 'Germany'
+				WHEN TRIM(CNTRY) is Null or TRIM(CNTRY) = '' THEN 'n/a'
+				ELSE TRIM(CNTRY)
+			END AS CNTRY
+		from bronze.erp_loc_a101;
+
+		set @end_time = GETDATE();
+		print 'load duration: ' + cast(datediff(second, @start_time,@end_time) AS VARCHAR) + '   seconds';
+
+		print 'truncating table silver.erp_px_cat_g1v2';
+		TRUNCATE TABLE silver.erp_px_cat_g1v2;
+
+		print 'inserting records into silver.erp_px_cat_g1v2';
+
+		set @start_time = GETDATE();
+
+		insert into silver.erp_px_cat_g1v2 (id,cat,subcat,maintenance)
+		select id,cat,subcat,maintenance
+		from  bronze.erp_px_cat_g1v2;
+
+		set @end_time = GETDATE();
+		print 'load duration: ' + cast(datediff(second, @start_time,@end_time) AS VARCHAR) + '   seconds';
+		set @silver_load_end = GETDATE();
+
+		print 'load duration for silver : ' + cast(datediff(second, @silver_load_start,@silver_load_end) AS VARCHAR) + '   seconds';
+
+	END TRY
+	BEGIN CATCH
+		print 'error occured during loading';
+		print 'error message ' + error_message();
+		print 'error meesage ' + cast(error_state() as varchar) ;
+	END catch
+END;
